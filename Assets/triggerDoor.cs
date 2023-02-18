7dc078601c76e75eb1af70ccf8775783ba89cdf8
HaleighZ <haleighzerkle@yahoo.com>
2023-02-17 22:04:28 -0500

    Tutorial level beautification
    Added floor and wall tiles, door sprite, terminal sprite, and gate sprite. Still need to fix the door opening script

--- Assets/triggerDoor.cs
+++ Assets/triggerDoor.cs
@@ -6,24 +6,47 @@ public class triggerDoor : MonoBehaviour
 {
     public BoxCollider2D door;
     private bool doorTrigger = true;
+    public BoxCollider2D terminal;
+
+    public BoxCollider2D player;
+
+    private Animator animator;
+
+    public bool Using;
+
+    
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        Using = false;
+
+        animator = GetComponent<Animator>();
     }
 
     // Update is called once per frame
-    void Update()
-    {
+    void Update(){
         if (Input.GetKeyDown("f")){
-            if (doorTrigger){
+            Using = true;
+        }
+        else{
+            Using = false;
+        }
+    }
+        void OnCollisionStay2D(BoxCollider2D col) {
+            
+            if(col.gameObject == player && Using){
                 door.enabled = false;
             }
-            else{
-                door.enabled = true;
-            }
-            doorTrigger = !doorTrigger;
-        }
+        
+        // if (Input.GetKeyDown("f")){
+        //     if (doorTrigger){
+        //         door.enabled = false;
+        //     }
+        //     else{
+        //         door.enabled = true;
+        //     }
+        //     doorTrigger = !doorTrigger;
+        // }
     }
 }
