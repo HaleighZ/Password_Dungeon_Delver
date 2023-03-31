a758fe975cc80b7eb7fe86e5e4d89986de3c24a8
HaleighZ <haleighzerkle@yahoo.com>
2023-03-31 01:04:02 -0400

    Made the drag and drop minigame functional
    I have both added a new script and modified an existing one to make this work.
    
    The HashcatVerifier has two vital functions that it performs to make this work, it can either Store to the public array it has, or it can Verify against the array of objects that has been deemed the solution for the puzzle. The solution array is determined by a list of objects dragged into it via the Unity GUI, which in this case is the order of which hashcat blocks you wish to be the answer. Currently it's just A,B,C,D but I plan to adjust that later.
    
    The Code Slot script was modified to hold both an index number and a Verifier script for each slot. The script is taken via dragging it over from the Scripts and the Index is to be typed in manually for each slot. This is very important for making sure the Verifier works correctly.

--- Assets/Scripts/CodeSlot.cs
+++ Assets/Scripts/CodeSlot.cs
@@ -5,10 +5,16 @@ using UnityEngine.EventSystems;
 
 public class CodeSlot : MonoBehaviour, IDropHandler
 {
-    //This will snap the dragged object into the center of this slot object when it is overlapping upon EndDrag
+    public HashcatVerifier Verifier;
+    public int index;
+    
     public void OnDrop(PointerEventData eventData){
         if(eventData.pointerDrag != null){
+            //This will snap the dragged object into the center of this slot object when it is overlapping upon EndDrag
             eventData.pointerDrag.GetComponent<RectTransform>().anchoredPosition = GetComponent<RectTransform>().anchoredPosition;
-        }
+            //This will store the entire data from the object being dragged into the verifier array, it will overwrite anything there previously
+            Verifier.Store(eventData.pointerDrag, index);
+        } 
     }
+    
 }
---
+++ Assets/Scripts/HashcatVerifier.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+
+public class HashcatVerifier : MonoBehaviour
+{
+    GameObject[] objects = {null, null, null, null};
+    public GameObject[] solution;
+
+    public void Verify(){
+        bool result = true;
+        for (int i = 0; i < objects.Length; i++)
+        {
+            if(objects[i] != solution[i]){
+                result = false;
+            }
+        }
+        if(result == true){
+            Debug.Log("You got it!");
+        }
+
+    }
+
+    public void Store(GameObject data, int index){
+        objects[index] = data;
+    }
+}
